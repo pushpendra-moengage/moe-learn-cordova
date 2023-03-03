0aa8de3d464d74f5628f56dabb115100b5f57a0e
Pushpendra <pushpendra.a@moengage.com>
2023-03-03 14:42:15 +0530

    moe sdk installation

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -15,12 +15,37 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/cordova-moengage-core/www/MoECordova.js",
+        "id": "cordova-moengage-core.MoECordova",
+        "pluginId": "cordova-moengage-core",
+        "clobbers": [
+            "MoECordova"
+        ]
+    },
+    {
+        "file": "plugins/cordova-moengage-core/www/MoEConstants.js",
+        "id": "cordova-moengage-core.MoEConstants",
+        "pluginId": "cordova-moengage-core",
+        "clobbers": [
+            "MoEConstants"
+        ]
+    },
+    {
+        "file": "plugins/cordova-moengage-core/www/MoEJsonProvider.js",
+        "id": "cordova-moengage-core.MoEJsonProvider",
+        "pluginId": "cordova-moengage-core",
+        "clobbers": [
+            "MoEJsonProvider"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-dialogs": "2.0.2"
+    "cordova-plugin-dialogs": "2.0.2",
+    "cordova-moengage-core": "8.3.0"
 }
 // BOTTOM OF METADATA
 });
