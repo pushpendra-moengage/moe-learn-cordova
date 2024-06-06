b10d4e1b81b73d2dfcc2160dbfce8211434a699e
A Pushpendra <aladupushpendra08@gmail.com>
2024-06-06 15:50:31 +0530

    added class

--- platforms/android/app/src/main/java/io/pin/core/hellocordova/MyApplication.java
+++ platforms/android/app/src/main/java/io/pin/core/hellocordova/MyApplication.java
@@ -12,6 +12,8 @@ import com.moengage.core.config.FcmConfig;
 import com.moengage.core.config.LogConfig;
 import com.moengage.core.config.NotificationConfig;
 import com.moengage.core.*;
+import com.moengage.core.DataCenter;
+
 
 
 public class MyApplication extends Application {
@@ -19,7 +21,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        MoEngage.Builder moEngage = new MoEngage.Builder(this, "YRRM72T6B2LAXARZC8TH9TGP")
+        MoEngage.Builder moEngage = new MoEngage.Builder(this, "YRRM72T6B2LAXARZC8TH9TGP", DataCenter.DATA_CENTER_1)
                                             .configureFcm(new FcmConfig(false))
                                             .configureLogs(new LogConfig(5, true))
                                             .setDataCenter(DataCenter.DATA_CENTER_3)
